1afc2b9ca1992d959032c28f6e3a65a9b184dd60
bdaanoun <daanouni.bilal@yahoo.com>
2025-12-06 16:20:54 +0100

    updates ...

--- backend/01blog/src/main/java/com/o1blog/_blog/controller/UserController.java
+++ backend/01blog/src/main/java/com/o1blog/_blog/controller/UserController.java
@@ -2,7 +2,7 @@ package com.o1blog._blog.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,8 +18,12 @@ import com.o1blog._blog.service.UserService;
 @RequestMapping("/api/users")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    // @Autowired
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping
     @ResponseBody
--- backend/01blog/src/main/java/com/o1blog/_blog/service/UserService.java
+++ backend/01blog/src/main/java/com/o1blog/_blog/service/UserService.java
@@ -2,13 +2,18 @@ package com.o1blog._blog.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.o1blog._blog.model.User;
 import com.o1blog._blog.repository.UserRepository;
 
+@Service
 public class UserService {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public User creatUser(User user) {
         return userRepository.save(user);
